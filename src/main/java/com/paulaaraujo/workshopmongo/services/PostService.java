f6208561571faf9775a713e2e84a1ac440c539a7
Paula Araujo <paula.araujo.dev@gmail.com>
2023-04-13 07:51:45 -0300

    obtendo post por id e criando endpoint de posts por usuário

---
+++ src/main/java/com/paulaaraujo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.paulaaraujo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulaaraujo.workshopmongo.domain.Post;
+import com.paulaaraujo.workshopmongo.repository.PostRepository;
+import com.paulaaraujo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
